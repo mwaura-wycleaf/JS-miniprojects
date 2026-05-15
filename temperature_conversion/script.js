90e100690759c4ec50151428a130287f36dcfd77
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-05-15 20:52:42 +0300

    seperated the html and javascript to different files

---
+++ temperature_conversion/script.js
@@ -0,0 +1,12 @@
+     function convertTemperature() {
+        let temp = document.querySelector("#tempConversion").value;
+        let unit = document.querySelector("#unitName").value;
+        let finalResult;
+        if (unit === "c=>f") {
+            finalResult = (parseFloat(temp) * 9/5) + 32 +" F";
+        }else if (unit === "f=>c"){
+            finalResult = (parseFloat(temp)- 32) * 9/5 +" C";
+        }
+        
+        document.querySelector("#demo").innerHTML = finalResult;
+    }
