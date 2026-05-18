3d54235ab15db18a186d1e440244d7a269ce4dae
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-05-18 21:56:00 +0300

    Added the second student as an object

--- student_management/script.js
+++ student_management/script.js
@@ -4,3 +4,10 @@ let student1 = {
     age: "19",
     course: "Computer Science"
 }
+
+let student2 = {
+    firstName: "Marco",
+    secondName: "Polo",
+    age: "24",
+    course: "Acturial Science"
+}
