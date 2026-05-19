9e74e8c1796668ffea6db1c366f95ef130895298
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-05-19 17:33:35 +0300

    Added a boolean isPresent to check on students' attendance

--- student_management/script.js
+++ student_management/script.js
@@ -2,12 +2,14 @@ let student1 = {
     firstName: "James",
     secondName: "Milner",
     age: "19",
-    course: "Computer Science"
+    course: "Computer Science",
+    isPresent: "true"
 }
 
 let student2 = {
     firstName: "Marco",
     secondName: "Polo",
     age: "24",
-    course: "Acturial Science"
+    course: "Acturial Science",
+    isPresent: "false"
 }
