db373437f845e0c56a3cf719de6e8ee9d0eac90a
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-06 20:50:36 +0300

    Created the initial logic for buuton access

---
+++ update count/script.js
@@ -0,0 +1,6 @@
+let start = document.getElementById("start");
+let reset = document.getElementById("reset");
+let reduce = document.getElementById("reduce");
+let result = document.getElementById("demo");
+let count = 0;
+let fontSize = 30;
