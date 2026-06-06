9f76b1225c6bf06c9d2414a962100ba88cd43f02
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-06 20:54:34 +0300

    Updated functionality for reduce count

--- update count/script.js
+++ update count/script.js
@@ -16,3 +16,15 @@ let fontSize = 30;
         result.style.color = "green";
         result.style.fontSize = fontSize + "px";
         });
+
+reduce.addEventListener("click", function() {
+            
+            if (count > 0){
+                count--;
+                fontSize -= 2;
+
+            }
+            updateCount();
+        result.style.color = "red";
+        result.style.fontSize = fontSize + "px";
+        });      
