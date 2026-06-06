f83ed7ed701def38b3d2392b809d7230a2647903
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-06 20:51:38 +0300

    Created my updateCount function to be reused

--- update count/script.js
+++ update count/script.js
@@ -4,3 +4,7 @@ let reduce = document.getElementById("reduce");
 let result = document.getElementById("demo");
 let count = 0;
 let fontSize = 30;
+
+ function updateCount () {
+         document.getElementById("demo").innerHTML = count;
+    }
