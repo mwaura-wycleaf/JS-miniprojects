66778144c3dfe6edd7bba2c4745d57c7b0617d7c
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-06 20:57:21 +0300

    Corrected the spacing

--- update count/script.js
+++ update count/script.js
@@ -5,32 +5,31 @@ let result = document.getElementById("demo");
 let count = 0;
 let fontSize = 30;
 
- function updateCount () {
-         document.getElementById("demo").innerHTML = count;
-    }
+function updateCount () {
+    document.getElementById("demo").innerHTML = count;
+}
 
-    start.addEventListener("click", function() {
-        count++;
-        fontSize += 2;
-        updateCount();
-        result.style.color = "green";
-        result.style.fontSize = fontSize + "px";
-        });
+start.addEventListener("click", function() {
+    count++;
+    fontSize += 2;
+    updateCount();
+    result.style.color = "green";
+    result.style.fontSize = fontSize + "px";
+    });
 
 reduce.addEventListener("click", function() {
             
-            if (count > 0){
-                count--;
-                fontSize -= 2;
-
-            }
-            updateCount();
-        result.style.color = "red";
-        result.style.fontSize = fontSize + "px";
-        });      
+    if (count > 0){
+        count--;
+        fontSize -= 2;
+   }
+    updateCount();
+    result.style.color = "red";
+    result.style.fontSize = fontSize + "px";
+});      
 
 reset.addEventListener("click", function() {
-        count = 0;
-        updateCount();
-        result.style.color = "black";
-        });
+    count = 0;
+    updateCount();
+    result.style.color = "black";
+ });
