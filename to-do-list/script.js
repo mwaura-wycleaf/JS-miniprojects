0eb5b0226b8403efe892d8e236c32b050d64fe1a
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-14 00:16:54 +0300

    Made the add task button functional

---
+++ to-do-list/script.js
@@ -0,0 +1,22 @@
+let task = [];
+
+function displayTask() {
+    let html = "";
+    for (let i = 0; i < task.length; i++) {
+        html += "<li>" + task[i] + "<button onclick = 'removeTask(" + i + ")';> x </button></li>"
+    }
+    document.getElementById("list").innerHTML = html;
+}
+
+function addTask() {
+    let theTask = document.getElementById("task");
+    let text= theTask.value;
+    
+    if (text === ""){
+        return;
+    }
+    task.push(text);
+    theTask.value = "";
+    displayTask()
+
+}
