080d687371ea952625b6bd6d6d11cdb018d04687
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-21 22:24:51 +0300

    Refactored my code to accomodate when time is below 10 be it hour, minutes or seconds

--- Digital_clock/script.js
+++ Digital_clock/script.js
@@ -10,6 +10,17 @@ function showTime () {
     minute.innerHTML = time.getMinutes();
     seconds.innerHTML = time.getSeconds();
     
+    if(hour.innerHTML < 10){
+        hour = "0" + hour.innerHTML;
+    }
+
+    if(minute.innerHTML < 10){
+        minute.innerHTML = "0" + minute.innerHTML;
+    }
+
+    if(seconds.innerHTML < 10){
+        seconds.innerHTML = "0" + seconds.innerHTML;
+    }
 
 }
 
