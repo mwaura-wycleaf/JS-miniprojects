f56939abd8e8069ceddd09156bc4a117de336a9f
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-21 22:18:01 +0300

    Adjusted the interval of seconds

--- Digital_clock/script.js
+++ Digital_clock/script.js
@@ -2,14 +2,16 @@ let hour = document.querySelector(".hour");
 let minute = document.querySelector(".minute");
 let seconds = document.querySelector(".seconds");
 
+setInterval(showTime, 1000)
 
 function showTime () {
     let time = new Date();
     hour.innerHTML = time.getHours();
     minute.innerHTML = time.getMinutes();
     seconds.innerHTML = time.getSeconds();
+    
 
 }
 
-showTime();
+
 
