62b39b78726e0e6fbc35bdff02d6f9778bc28af7
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-06-24 19:16:29 +0300

    refactor:switch to addEventListener for event handling

--- random_bg_color/script.js
+++ random_bg_color/script.js
@@ -1,3 +1,5 @@
-function changeColor() {
+let myButton = document.getElementById("myButton")
+
+myButton.addEventListener("click", function() {
     document.body.style.backgroundColor = `rgb(${Math.round(Math.random()* 225)}, ${Math.round(Math.random()* 225)}, ${Math.round(Math.random()* 225)} )`
-}
+})
