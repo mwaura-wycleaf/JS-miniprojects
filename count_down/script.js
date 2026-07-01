5f460c0a1b0873a1e7bf5b72eef2d8d13f0a7742
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-01 18:48:44 +0300

    feat: Add end date for countdown functionality in script.js

--- count_down/script.js
+++ count_down/script.js
@@ -2,3 +2,6 @@ let dayBox = document.getElementById("day-box");
 let hourBox = document.getElementById("hour-box");
 let minuteBox = document.getElementById("min-box");
 let secondBox = document.getElementById("sec-box");
+
+//Format: Date(year, month, day, hours, minutes, seconds)
+let endDate = new Date(2026,8,3,16,30);
