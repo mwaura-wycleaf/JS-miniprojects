7e99176af725e7ba280cfc5bb8059cf247901d92
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-01 18:36:48 +0300

    feat: Initialize countdown box elements in script.js

---
+++ count_down/script.js
@@ -0,0 +1,4 @@
+let dayBox = document.getElementById("day-box");
+let hourBox = document.getElementById("hour-box");
+let minuteBox = document.getElementById("min-box");
+let secondBox = document.getElementById("sec-box");
