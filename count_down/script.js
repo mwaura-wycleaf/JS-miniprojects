debc752de87c59bf07b8929a3197cacc6b1596a2
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-02 16:40:32 +0300

    feat: Implement countdown functionality with time calculations and display updates

--- count_down/script.js
+++ count_down/script.js
@@ -5,3 +5,45 @@ let secondBox = document.getElementById("sec-box");
 
 //Format: Date(year, month, day, hours, minutes, seconds)
 let endDate = new Date(2026,8,3,16,30);
+//Outputs the value in milliseconds
+let endTime = endDate.getTime();
+
+function countDown() {
+    let todayDate = new Date();
+    let todayTime = todayDate.getTime();
+    let timeRemaining = endTime - todayTime;
+    
+ 
+    //To milliseconds
+    let oneMinute = 60 * 1000;
+    let oneHour = 60 * oneMinute;
+    let oneDay = 24 * oneHour;
+
+    let addZeros = (num) => {
+        if(num < 10){
+            return "0" + num;
+        }
+        return num;
+    };
+
+    //if end date is before today date, then countdown has ended
+    if (endTime < todayTime) {
+      document.querySelector(".container").innerHTML = "<h1>Countdown has ended!</h1>";
+    }
+
+    //if end date is after today date, then countdown is still running
+
+    else{
+        let daysleft = Math.floor(timeRemaining / oneDay);
+        let hoursleft = Math.floor((timeRemaining % oneDay) / oneHour);
+        let minutesleft = Math.floor((timeRemaining % oneHour) / oneMinute);
+        let secondsleft = Math.floor((timeRemaining % oneMinute) / 1000);
+        dayBox.innerHTML = addZeros(daysleft);
+        hourBox.innerHTML = addZeros(hoursleft);
+        minuteBox.innerHTML = addZeros(minutesleft);
+        secondBox.innerHTML = addZeros(secondsleft);
+    }
+
+
+}
+setInterval(countDown, 1000);
