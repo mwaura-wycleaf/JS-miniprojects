bac03b3c2ae736a54be62a1e6109217400e508e8
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-03 17:43:45 +0300

    feat: Implement text-to-speech functionality with event listener for submit button

--- text_converter_to speech/script.js
+++ text_converter_to speech/script.js
@@ -3,3 +3,18 @@ let submit = document.getElementById("submit");
 let resume = document.getElementById("resume");
 let pause = document.getElementById("pause");   
 let audio;
+
+submit.addEventListener("click", function(){
+    audio.text = text.value;
+
+    //speak the text
+    window.speechSynthesis.speak(audio);
+});
+
+window.onload = function(){
+    if ('speechSynthesis' in window) {
+        audio = new SpeechSynthesisUtterance();
+    } else {
+        alert("Sorry, your browser doesn't support text to speech!");
+    }
+}
