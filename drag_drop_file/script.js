4f284c656edeced57119501d30efb3e4582a0657
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-05 17:11:29 +0300

    feat: Initialize variables for drag and drop file upload functionality

---
+++ drag_drop_file/script.js
@@ -0,0 +1,6 @@
+let uploadButton = document.getElementById("upload-button");
+let chosenImg = document.getElementById("chosen-image");
+let fileName = document.getElementById("file-name");
+let container = document.querySelector("container");
+let error = document.getElementById("error");
+let imgDisplay = document.getElementById("image-display");
