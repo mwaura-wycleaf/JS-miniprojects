878eaad57fba24361e4778410ece993dda325cbf
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-05 17:24:24 +0300

    feat: Implement file handling for image uploads with error handling and display

--- drag_drop_file/script.js
+++ drag_drop_file/script.js
@@ -4,3 +4,24 @@ let fileName = document.getElementById("file-name");
 let container = document.querySelector("container");
 let error = document.getElementById("error");
 let imgDisplay = document.getElementById("image-display");
+
+const fileHandler = (file, name, type) => {
+    if (type.split("/")[0] !== "image"){
+        //File type error
+        error.innerText = "Please Upload an image file"
+        return false;
+    }
+    error.innerText = "";
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+     let imageContainer = document.createElement("figure");
+     let img = document.createElement("img");
+     img.src = reader.result;
+     imageContainer.appendChild(img);
+     imageContainer.innerHTML += `<figcaption>${name}</figcaption>`;
+     imageDisplay.appendChild(imageContainer);
+    }
+}
+
+
