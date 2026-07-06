fe11234a0f1b94cce8d73c9430003e6d8f42e992
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-06 13:09:22 +0300

    fix: Update button event listener for email masking functionality

--- email_masking/script.js
+++ email_masking/script.js
@@ -1,15 +1,17 @@
-   function clickButton() {
-           let myEmail = document.querySelector("#yourEmail").value;
-           let starsCount = Math.max(0, myEmail.length - 12)
-           let stars = "*".repeat(starsCount)
-           let newEmail = myEmail.slice(0,2) + stars + myEmail.slice(-10);
-           
+let maskEmail = document.getElementById("maskEmail")
 
-           if (myEmail === ""){
-            document.querySelector("#demo").innerHTML = "Enter email!!";
-             document.querySelector("#demo").style.color = "red";
-        }else {
-            document.querySelector("#demo").innerHTML = newEmail;
-            document.querySelector("#demo").style.color = "black";
-        }
-        }
+maskEmail.addEventListener("click", function clickButton() {
+    let myEmail = document.querySelector("#yourEmail").value;
+    let starsCount = Math.max(0, myEmail.length - 12)
+    let stars = "*".repeat(starsCount)
+    let newEmail = myEmail.slice(0,2) + stars + myEmail.slice(-10);
+        
+
+    if (myEmail === ""){
+        document.querySelector("#demo").innerHTML = "Enter email!!";
+            document.querySelector("#demo").style.color = "red";
+    }else {
+        document.querySelector("#demo").innerHTML = newEmail;
+        document.querySelector("#demo").style.color = "black";
+    }
+})
