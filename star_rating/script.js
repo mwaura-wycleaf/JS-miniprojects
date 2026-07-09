4ebd1bc9a3574721446efbabb9d9d07720f8a519
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-09 10:18:44 +0300

    feat: Add initial JavaScript setup for star rating feature

---
+++ star_rating/script.js
@@ -0,0 +1,4 @@
+let startContainer = document.querySelector(".star-container");
+let submitButton = document.querySelector("#submit");
+let message = document.querySelector("#message");
+let submitSection = document.querySelector("submitSection");
