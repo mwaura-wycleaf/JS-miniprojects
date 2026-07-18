8d3624c49f8204cc1e85af6b046ce656cbf18942
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-18 22:20:30 +0300

    refactor: Remove email masking functionality and prepare for new implementation

--- email_masking/script.js
+++ email_masking/script.js
@@ -1,25 +1,6 @@
 let maskEmail = document.getElementById("maskEmail")
 let email = document.getElementById("email");
 
-maskEmail.addEventListener("click", function clickButton() {
-    let myEmail = email.value;
-    let starsCount = Math.max(0, myEmail.length - 12)
-    let stars = "*".repeat(starsCount)
-    let newEmail = myEmail.slice(0,2) + stars + myEmail.slice(-10);
-        
-
-    if (myEmail === ""){
-        document.querySelector("#demo").innerHTML = "Enter email!!";
-            document.querySelector("#demo").style.color = "red";
-    }else {
-        document.querySelector("#demo").innerHTML = newEmail;
-        document.querySelector("#demo").style.color = "black";
-    }
-})
-
-email.addEventListener("keydown", (event) => {
-    if (event.key === "Enter"){
-        event.preventDefault();
-        maskEmail.click();
-    }
-})
+function myEmail() {
+    let emailIndex = email.indexof("@");
+}
