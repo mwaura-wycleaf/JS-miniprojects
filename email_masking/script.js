b22d8f90057225a6ffda41ea1f7fb5e2dac74d1a
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-18 22:43:26 +0300

    fix: Correct indexof to indexOf and update email masking logic

--- email_masking/script.js
+++ email_masking/script.js
@@ -2,7 +2,10 @@ let maskEmail = document.getElementById("maskEmail")
 let email = document.getElementById("email");
 
 function myEmail() {
+    let emailValue = email.value;
     let emailIndex = email.indexof("@");
     let username = email.slice(0, emailIndex);
-    let end = email.slice(emailIndex);
+    let domain = email.slice(emailIndex);
+
+    let starsCount = "*".repeat(username.length - 2);
 }
