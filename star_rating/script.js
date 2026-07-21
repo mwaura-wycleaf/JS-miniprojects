97a188fd996d931e775da658cd6d77466a5b2fab
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-21 15:32:33 +0300

    fix: Remove unused star rating logic and simplify event handling

--- star_rating/script.js
+++ star_rating/script.js
@@ -1,70 +1,11 @@
-let startContainer = document.querySelectorAll(".star-container");
+let star = document.querySelectorAll(".star-container");
 let submitButton = document.querySelector("#submit");
 let message = document.querySelector("#message");
 let submitSection = document.querySelector(".submit-section");
 
 const messages = ["", "Terrible", "Poor", "Average", "Good", "Excellent"];
 
-startContainer.forEach((element, index) => {
-    element.addEventListener(event[deviceType].over, () => {
-        submitButton.disabled = false;
-        if(element.classList.contains("inactive")){
-            //Fill star
-            ratingUpdate(0, index, true);
-        } else {
-            //Regular stars (remove color)
-            ratingUpdate(index, startContainer.length-1, false);
-        }
-    })
-})
 
-const ratingUpdate = (start, end, active) => {
-    for (let i = start ; i<=end; i++) {
-        if(active) {
-            startContainer[i].classList.remove("inactive");
-            startContainer[i].firstElementChild.className = "fa-star fa-solid"
-            
-        }else {
-            startContainer[i].classList.add("inactive");
-            startContainer[i].firstElementChild.className = "fa-star fa-regular"
-        }
-    }
-
-    // Count stars that are filled
-        let activeCount = 0;
-        startContainer.forEach(star => {
-            if (!star.classList.contains("inactive")) {
-                activeCount++;
-            }
-        });
-
-        if (activeCount > 0){
-            switch (activeCount) {
-            case 1:
-            message.innerText = "Terrible";
-            break;
-
-            case 2:
-             message.innerText = "Poor";
-            break;
-
-            case 3:
-             message.innerText = "Average";
-            break;
-
-            case 4:
-             message.innerText = "Good";
-            break;
-
-            case 5:
-             message.innerText = "Excellent";
-            break;
-        }
-    }
-    else {
-        message.innerText = "";
-    }
-};
 
 submitButton.addEventListener("click", () => {
     submitSection.classList.remove("hide");
