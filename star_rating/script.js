62a548cd6d40dda5216c699c5935d7df0ae9e625
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-07-21 15:33:33 +0300

    fix: Implement star rating click event handling and update message display

--- star_rating/script.js
+++ star_rating/script.js
@@ -6,6 +6,28 @@ let submitSection = document.querySelector(".submit-section");
 const messages = ["", "Terrible", "Poor", "Average", "Good", "Excellent"];
 
 
+stars.forEach((clickedStar, clickedIndex) => {
+  clickedStar.addEventListener("click", () => {
+    
+    submitButton.disabled = false;
+
+
+    stars.forEach((star, index) => {
+      const icon = star.firstElementChild;
+
+      if (index <= clickedIndex) {
+        star.classList.remove("inactive");
+        icon.className = "fa-star fa-solid";  // Filled star
+      } else {
+        star.classList.add("inactive");
+        icon.className = "fa-star fa-regular"; // Empty star
+      }
+    });
+
+    
+    message.innerText = messages[clickedIndex + 1];
+  });
+});
 
 submitButton.addEventListener("click", () => {
     submitSection.classList.remove("hide");
