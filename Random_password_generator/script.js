b7fb1b683f3f83c65f6e8eac3d17bac3d69ba478
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-08-02 19:32:27 +0300

    feat: Add password generation functionality and update output display

--- Random_password_generator/script.js
+++ Random_password_generator/script.js
@@ -1,3 +1,14 @@
 const characters = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:'",.<>/?\`\\~ `;
 let output = document.getElementById("output");
 
+function genPassword(){
+    let length = document.getElementById("length").value;
+    document.getElementById("length-value").innerText = length;
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    output.value = password;
+}
+
+genPassword();
