0d3ae1a120b93646ecc46f0fcb1be55332cb4c48
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-08-07 10:08:03 +0300

    feat: Implement geolocation functionality on location button click

--- user_location/script.js
+++ user_location/script.js
@@ -1,3 +1,10 @@
 let locationButton = document.getElementById("get-location");
 let locationDetails = document.getElementById("location-details");
 
+locationButton.addEventListener("click", function() {
+ if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, checkError);;
+ } else {
+    locationDetails.innerText = "Geolocation is not supported by this browser.";
+ }
+});
