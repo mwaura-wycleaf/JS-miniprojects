9be8a8fff1bcc2133ad8e05c3709f5d2e2fac6d2
mwaura-wycleaf <wycliff.14396@gmail.com>
2026-08-07 10:11:41 +0300

    feat: Add geolocation success and error handling functions

--- user_location/script.js
+++ user_location/script.js
@@ -8,3 +8,26 @@ locationButton.addEventListener("click", function() {
     locationDetails.innerText = "Geolocation is not supported by this browser.";
  }
 });
+
+const showPosition = (position) => {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    locationDetails.innerText = `Latitude: ${latitude}, Longitude: ${longitude}`;
+}
+
+const checkError = (error) => {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            locationDetails.innerText = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            locationDetails.innerText = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            locationDetails.innerText = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            locationDetails.innerText = "An unknown error occurred.";
+            break;
+    }
+}
